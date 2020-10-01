0f13c44d82da199cb667594c87dc1711e80b4f07
st-it-01 <ilyasov_as@creacept.ru>
2020-10-01 16:22:42 +0300

    v1.0.0

---
+++ portfolio/data.php
@@ -0,0 +1,115 @@
+<?php
+
+return [
+    'photo' => 'https://pngimg.com/uploads/mouth_smile/mouth_smile_PNG27.png',
+    'about' => [
+        'name' => 'Ilyasov Aleksander',
+        'post' => 'Developer',
+        'email' => 'velmoren@gmail.com',
+        'phone' => '+375257183077',
+        'site' => 'google.com'
+    ],
+    'education' => [
+        [
+            'faculty' => 'Random Faculty1',
+            'university' => 'Some University1',
+            'yearStart' => 2015,
+            'yearEnd' => 2018
+        ],
+        [
+            'faculty' => 'Random Faculty2',
+            'university' => 'Some University2',
+            'yearStart' => 2010,
+            'yearEnd' => 2015
+        ]
+    ],
+    'languages' => [
+        'English', 'Russian', 'Spanish'
+    ],
+    'interests' => [
+        'interest1', 'interest2'
+    ],
+    'aboutCareer' => 'lorem lorem lorem',
+    'career' => [
+        [
+            'post' => 'post 1',
+            'place' => 'city 1',
+            'yearStart' => 2010,
+            'yearEnd' => 2012,
+            'duty' => 'lorem lorem lorem 1'
+        ],
+        [
+            'post' => 'post 2',
+            'place' => 'city 1',
+            'yearStart' => 2015,
+            'yearEnd' => 2020,
+            'duty' => 'lorem lorem lorem 2'
+        ],
+        [
+            'post' => 'post 3',
+            'place' => 'city 3',
+            'yearStart' => 2012,
+            'yearEnd' => 2015,
+            'duty' => 'lorem lorem lorem 3'
+        ]
+    ],
+    'experiences' => [
+        [
+            'post' => 'post 1',
+            'yearStart' => 2010,
+            'yearEnd' => 2012,
+            'description' => 'lorem lorem lorem lorem 1',
+            'text' => 'Lorem lorem lorem lorem 1'
+        ],
+        [
+            'post' => 'post 2',
+            'yearStart' => 2015,
+            'yearEnd' => 2020,
+            'description' => 'lorem lorem lorem lorem 2',
+            'text' => 'Lorem lorem lorem lorem 2'
+        ]
+    ],
+    'projectsInfo' => [
+        'title' => 'lorem lorem lorem',
+        'projects' => [
+            [
+                'link' => 'google.com',
+                'linkText' => 'Proj 1',
+                'text' => 'lorem lorem lorem 1'
+            ],
+            [
+                'link' => 'google.com',
+                'linkText' => 'Proj 2',
+                'text' => 'lorem lorem lorem 2'
+            ],
+            [
+                'link' => 'google.com',
+                'linkText' => 'Proj 3',
+                'text' => 'lorem lorem lorem 3'
+            ],
+            [
+                'link' => 'google.com',
+                'linkText' => 'Proj 4',
+                'text' => 'lorem lorem lorem 4'
+            ]
+        ]
+    ],
+    'skills' => [
+        [
+            'name' => 'Javascript & jQuery',
+            'percent' => 98
+        ],
+        [
+            'name' => 'HTML5 & CSS',
+            'percent' => 85
+        ],
+        [
+            'name' => 'React.JS',
+            'percent' => 60
+        ],
+        [
+            'name' => 'Next.JS',
+            'percent' => 45
+        ]
+    ]
+];

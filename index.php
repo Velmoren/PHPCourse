fa2fe9e980d18f788a4318db1943fcbcc0e26e0c
st-it-01 <ilyasov_as@creacept.ru>
2020-10-05 12:18:02 +0300

    v1.0.0

--- index.php
+++ index.php
@@ -5,12 +5,24 @@ class Product
     public $title;
     public $price;
     public $weight;
+    public $nds;
 
-    public function __construct($title, $price, $weight)
+    public function __construct($title, $price, $weight, $nds)
     {
         $this->title = $title;
         $this->price = $price;
         $this->weight = $weight;
+        $this->nds = $nds;
+    }
+
+    public function printPrice()
+    {
+        return $this -> price;
+    }
+
+    public function printNDSPrice()
+    {
+        return $this->printPrice() - $this->nds;
     }
 
     public function printArticle()
@@ -18,18 +30,21 @@ class Product
         echo
         "<div style='width: 200px;height: 200px;border: 1px solid black;margin-bottom: 20px'>
           <h2 style='text-align: center'>{$this->title}</h2>
-          <h3 style='text-align: center'>{$this->price} руб.</h3>
+          <h3 style='text-align: center'>{$this->printPrice()} руб.</h3>
+          <h3 style='text-align: center'>{$this->printNDSPrice()} руб. без НДС</h3>
           <h3 style='text-align: center'>{$this->weight} грамм.</h3>
         </div>";
     }
 
+
+
 }
 
-$productItem = new Product('Болт', 20, '60');
+$productItem = new Product('Болт', 20, '60', '10');
 $productItem->printArticle();
 
-$productItem = new Product('Винт', 30, '100');
+$productItem = new Product('Винт', 30, '100', '12');
 $productItem->printArticle();
 
-$productItem = new Product('Шпунт', 40, '120');
+$productItem = new Product('Шпунт', 40, '120', '7');
 $productItem->printArticle();

c28bca372774156b2f796ffa9c33a61406e70009
st-it-01 <ilyasov_as@creacept.ru>
2020-10-05 17:06:49 +0300

    d

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-abstract class Product
+class Product
 {
+    static $companyName = 'Мороженко_и_Ко';
+    const YEAR_START = '1987г';
+
     public $title;
     public $width;
     public $height;
     public $img;
-    public $count = 0;
 
     public function __construct(string $title, int $width, int $height, string $img)
     {
@@ -15,44 +17,21 @@ abstract class Product
         $this->height = $height;
         $this->img = $img;
     }
-    public function incCount()
-    {
-        $this->count++;
-    }
-    abstract public function showImage();
-}
-
-class Chocolate extends Product
-{
-    public $cal;
-
-    public function __construct(string $title, int $width, int $height, string $img, int $cal)
-    {
-        $this->cal = $cal;
-        parent::__construct($title, $width, $height, $img);
-    }
 
     public function showImage()
     {
-        $this->incCount();
         echo
         "<div style='display:flex; flex-direction: column; align-items: center; justify-content: space-between; background: url({$this->img}) no-repeat center/100%;width: {$this->width}px;height: {$this->height}px;border: 1px solid black;margin-bottom: 20px'>
-          <h2 style='text-align: center; margin: 0;'>{$this->title}{$this->count}</h2>
-          <h3 style='text-align: center; margin: 0;'>{$this->cal} ККАЛ</h3>
+          <h2 style='text-align: center; margin: 0;'>{$this->title}</h2>
         </div>";
     }
 }
 
-//class Candy extends Product
-//{
-//    public function __construct(string $title, int $width, int $height, string $img)
-//    {
-//        parent::__construct($title, $width, $height, $img);
-//    }
-//}
 
-$choco = new Chocolate('Choco', 200, 200,'choco.png', 120);
+
+
+$choco = new Product('Choco', 200, 200,'choco.png');
 $choco->showImage();
+echo Product::$companyName . " " . Product::YEAR_START;
 
-//$cand = new Candy('Konfeta', 200, 200,'konfeta.png');
 
